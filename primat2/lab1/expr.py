a2dbf2a8be8d16817716f1cf4c2a7525fb916bc8
MegaBluejay <dmoiseev2011@gmail.com>
2022-12-15 10:07:09 +0300

    add ineq handling

--- primat2/lab1/expr.py
+++ primat2/lab1/expr.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.optimize import linprog
-from sympy import Expr, init_printing, symbols
+from sympy import Expr, Le, init_printing, symbols
 
 from .simplex import simplex
 
@@ -10,17 +10,29 @@ x1, x2, x3, x4, x5 = _x = symbols([f"x_{i}" for i in range(1, 6)])
 
 
 def to_list(expr: Expr, n):
-    coeffs = [expr.coeff(_x[i]) for i in range(n)]
-    coeffs.append(expr.as_coeff_add()[0])
-    return list(map(float, coeffs))
+    return [float(expr.coeff(_x[i])) for i in range(n)]
 
 
 def solve(ntest, n, objective, constraints, basic, direction="min"):
-    f = np.array(to_list(objective, n)[:-1])
+    f = np.array(to_list(objective, n))
     if direction != "min":
         f *= -1
-    a = np.array([to_list(constraint, n) for constraint in constraints])
-    a[:, -1] *= -1
+    # a = np.array([to_list(constraint, n) for constraint in constraints])
+    a = []
+    ntot = n
+    for constraint in constraints:
+        if isinstance(constraint, Expr):
+            row = to_list(constraint, n)
+            row.extend([0] * (ntot - n))
+            row.append(-float(constraint.as_coeff_Add()[0]))
+        else:
+            row = to_list(constraint.lhs, n)
+            row.extend([0] * (ntot - n))
+            row.append(1 if isinstance(constraint, Le) else -1)
+            row.append(float(constraint.rhs))
+            ntot += 1
+        a.append(row)
+    a = np.array(a)
     ans = simplex(a, f, np.array(basic) - 1)
     correct = linprog(f, A_eq=a[:, :-1], b_eq=a[:, -1]).x
     ok = ans is not None and np.allclose(ans, correct)
